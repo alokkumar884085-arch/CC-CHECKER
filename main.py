b6598a6cbd4031bcdb01ec3e1d5afa05b3d3c73a
alokkumar884085-arch <alokkumar884085@gmail.com>
2026-08-04 17:51:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -589,7 +589,7 @@ def main():
     
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("stop", stop_bot))
-    app.add_handler(CommandHandler("startall", start_bot))
+    app.add_handler(CommandHandler("startall", stop_bot))
     app.add_handler(CommandHandler("admin", admin_pannel))
     app.add_handler(CommandHandler("adminpannel", admin_pannel))
     app.add_handler(CommandHandler("makeadmin", make_admin))
